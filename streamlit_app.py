03e2c89744fdadb11bdb16f80924416bebca5039
HeadBrocolli <109845194+HeadBrocolli@users.noreply.github.com>
2022-07-23 12:53:21 +0530

    Update streamlit_app.py
    Trying few options in code

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-'My Parents New Healthy Diner'
+"My Parents New Healthy Diner"
 
-
+'Omega 3 and Blue Berry Oatmeal'
+'Kale, Spinach and Rocket Smoothie'
+'Hard Boiled Free range Egg'
 
