1116108bca7d6241d8bac5fe22337e0cd1987878
HeadBrocolli <109845194+HeadBrocolli@users.noreply.github.com>
2022-07-23 13:02:08 +0530

    Update streamlit_app.py
    Adding EMOJIS

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,10 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-streamlit.text('Avocados Toast')
+streamlit.text('Omega 3 & Blueberry Oatmeal 🥣')
+streamlit.text('Kale, 🥗 Spinach & Rocket Smoothie🍞')
+streamlit.text('Hard-Boiled 🐔 Free-Range Egg')
+streamlit.text('🥑Avocados Toast')
 streamlit.header('Build your own food')
+   
 
