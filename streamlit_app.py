8dffc7ac797fea8a516bd3b0c39c2a2fdf45fd58
HeadBrocolli <109845194+HeadBrocolli@users.noreply.github.com>
2022-07-23 13:05:43 +0530

    Update streamlit_app.py
    Adding Pandas Library

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -9,4 +10,5 @@ streamlit.text('Hard-Boiled 🐔 Free-Range Egg')
 streamlit.text('🥑Avocados Toast')
 streamlit.header('Build your own food')
    
-
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
