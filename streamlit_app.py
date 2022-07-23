dc9280d686fa710fe0896a221f13d5f06f5e3f51
HeadBrocolli <109845194+HeadBrocolli@users.noreply.github.com>
2022-07-23 13:00:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,10 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('My Parents New Healthy Diner')
-
-streamlit.text('Omega 3 and Blue Berry Oatmeal')
-streamlit.text('Kale, Spinach and Rocket Smoothie')
-streamlit.text('Hard Boiled Free range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Avocados Toast')
+streamlit.header('Build your own food')
 
