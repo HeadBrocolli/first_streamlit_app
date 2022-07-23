ed6dae2ce3d690dcb5417ac661d8c0a881f56d8b
HeadBrocolli <109845194+HeadBrocolli@users.noreply.github.com>
2022-07-23 12:56:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.Header("My Parents New Healthy Diner")
+streamlit.Header('My Parents New Healthy Diner')
 
 streamlit.text('Omega 3 and Blue Berry Oatmeal')
 streamlit.text('Kale, Spinach and Rocket Smoothie')
